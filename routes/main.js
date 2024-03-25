4948d4c7e613083655f7c86c8aac9c92dd59324d
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-26 00:58:02 +0200

    testing authMiddleware

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { dashboard, login } = require("../controllers/main");
+const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/login", login);
-router.get("/dashboard", dashboard);
+router.get("/dashboard", authMiddleware, dashboard);
 
 module.exports = router;
