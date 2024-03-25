8e6e2c2addbc2091cfe3aa008a96b8cc0a5f40f6
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-26 00:16:17 +0200

    adding jwt and sign tokens upon login

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,17 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const CustomError = require("../errors/custom-error");
 const login = async (req, res) => {
-  res.send("fake login/register/signup route");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomError("please provide username and password", 400);
+  }
+  const id = new Date().toLocaleTimeString();
+
+  const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {
+    expiresIn: "10d",
+  });
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
