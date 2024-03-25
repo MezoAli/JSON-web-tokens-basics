4550e28bfabe93169427f4e04f026756e9cdb0fc
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-03-26 01:19:23 +0200

    create Auth middleware function

--- controllers/main.js
+++ controllers/main.js
@@ -1,35 +1,30 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const CustomError = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    throw new CustomError("please provide username and password", 400);
+    throw new CustomError(
+      "please provide username and password",
+      StatusCodes.BAD_REQUEST
+    );
   }
   const id = new Date().toLocaleTimeString();
 
   const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {
     expiresIn: "10d",
   });
-  res.status(200).json({ msg: "user created", token });
+  res.status(StatusCodes.OK).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const { authorization } = req.headers;
-  const [_, token] = authorization.split(" ");
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    throw new CustomError("no token found", 401);
-  }
-  try {
-    const data = jwt.verify(token, process.env.JWT_SECRET);
-    const number = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `hello ${data.username}`,
-      secret: `your number is ${number}`,
-    });
-  } catch (error) {
-    throw new CustomError("not authorized to access that route data", 401);
-  }
+  const number = Math.floor(Math.random() * 100);
+
+  const { username } = req.user;
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `hello ${username}`, secret: `your number is ${number}` });
 };
 
 module.exports = {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,23 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const CustomError = require("../errors/custom-error");
+
 const authMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  next();
+  const { authorization } = req.headers;
+  const [_, token] = authorization.split(" ");
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    throw new CustomError("no token found", 401);
+  }
+  try {
+    const { username, id } = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = {
+      username,
+      id,
+    };
+    next();
+  } catch (error) {
+    throw new CustomError("not authorized to access that route data", 401);
+  }
 };
 
 module.exports = authMiddleware;
